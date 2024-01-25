7aef76224fbe7d9894ea42636431e8d39cf7b2e0
dsabirbaev <davran9559@gmail.com>
2024-01-25 14:37:02 +0500

    add render data

--- pages/documents.vue
+++ pages/documents.vue
@@ -6,6 +6,7 @@
 
     import { useAuthStore } from '~/store/auth.store';
     import { useStatusQuery } from '~/query/use-status-query';
+import type { card } from '@nuxt/ui';
 
     definePageMeta({ layout: "documents" })
     useHead({ title: "Documents | Jira software" })
@@ -30,17 +31,38 @@
 
     const { data } = useStatusQuery()
 
-    console.log(data)
+    
 </script>
 
 <template>
     <div class="grid grid-cols-4 gap-2 mt-12">
-        <UButton class="w-full h-12" color="blue" variant="outline" v-for="(item, index) in status" :key="index">
-            <div class="flex items-center space-x-2">
-                <span class="font-bold">{{ item.name }}</span>
-                <span class="text-sm text-neutral-500">2</span>
+        <div v-for="item in data" :key="item.id">
+            <UButton class="w-full h-12" color="blue" variant="outline">
+                <div class="flex items-center space-x-2">
+                    <span class="font-bold">{{ item.name }}</span>
+                    <span class="text-sm text-neutral-500">{{ item.items.length }}</span>
+                </div>
+            </UButton>
+
+            <div class="my-3 bg-gray-900 rounded-md p-2" v-for="card in item.items" :key="card.$id" role="button" draggable="true">
+                <div class="flex items-center space-x-2">
+                    <span class="font-bold text-lg uppercase">{{ card.name }}</span>
+                </div>
+
+                <UDivider class="my-3" />
+
+                <div class="opacity-55 text-sm line-clamp-1">
+                    {{ card.description }}
+                </div>
+
+                <UButton color="blue" class="w-full mt-3 group" variant="ghost"> 
+                    <span class="font-bold">More details</span>
+
+                    <Icon name="material-symbols:arrow-right-alt-rounded" class="group-hover:translate-x-2 transition"/>
+                </UButton>
             </div>
-        </UButton>
+        </div>
+       
         
     </div>
 </template>
