a5a2167c4adb4949aa2dedce7781694359689eef
dsabirbaev <davran9559@gmail.com>
2024-01-26 20:31:45 +0500

    add slideover

--- pages/documents.vue
+++ pages/documents.vue
@@ -5,14 +5,18 @@
     import { COLLECTION_DEALS, DB_ID } from '~/constants';
     import { DATABASE } from '~/libs/appwrite';
     import { useStatusQuery } from '~/query/use-status-query';
+    import { useCurrentDealStore } from "~/store/current-deal.store"
+    import type { IColumn, IDeal } from '~/types';
    
 
     definePageMeta({ layout: "documents" })
     useHead({ title: "Documents | Jira software" })
-    const { data, isLoading, refetch } = useStatusQuery()
 
-    const dragCardRef = ref(null)
-    const sourceColumnRef = ref(null)
+    const { data, isLoading, refetch } = useStatusQuery()
+    const { set } = useCurrentDealStore()
+
+    const dragCardRef = ref<IDeal | null>(null)
+    const sourceColumnRef = ref<IColumn | null>(null)
     const isMoving = ref(false)
 
     const {mutate, isPending} = useMutation({
@@ -22,9 +26,24 @@
         onSuccess: () => refetch(),
     })
 
-    const handleDragOver = () => {
-
+    const handleDragStart = (card: IDeal, column: IColumn) => {
+        isMoving.value = true
+        dragCardRef.value = card
+        sourceColumnRef.value = column
     }
+
+    const handleDragOver = (event: DragEvent) => {
+        event.preventDefault()
+    }
+
+    const handleDrop = (column: IColumn) => {
+        isMoving.value = false
+        if(dragCardRef.value && sourceColumnRef.value){
+            mutate({ docId: dragCardRef.value.$id, status: column.id})
+        }
+    }
+
+    
 </script>
 
 <template>
@@ -43,7 +62,11 @@
 
     <div class="grid grid-cols-4 gap-2 mt-12" v-else>
         
-        <div v-for="column in data" :key="column.id">
+        <div v-for="column in data" :key="column.id" 
+             @dragover="handleDragOver" 
+             @drop="() => handleDrop(column)"
+             class="px-1"
+             :class="isMoving && 'border-l-2 border-r-2 border-dotted h-screen dark:border-gray-900 border-gray-200'">
             <UButton class="w-full h-12" color="blue" variant="outline">
                 <div class="flex items-center space-x-2">
                     <span class="font-bold">{{ column.name }}</span>
@@ -53,18 +76,21 @@
 
             <SharedCreateDeal :status="column.id" :refetch="refetch"/>
 
-            <div class="my-3 bg-gray-900 rounded-md p-2" v-for="card in column.items" :key="card.$id" role="button" draggable="true">
+            <div class="my-3 bg-gray-900 rounded-md p-2" v-for="deal in column.items" :key="deal.$id" role="button" 
+                 draggable="true" 
+                 @dragstart="() => handleDragStart(deal, column)"
+                 :class="isPending && 'opacity-50 cursor-not-allowed'">
                 <div class="flex items-center space-x-2">
-                    <span class="font-bold text-lg uppercase">{{ card.name }}</span>
+                    <span class="font-bold text-lg uppercase">{{ deal.name }}</span>
                 </div>
 
                 <UDivider class="my-3" />
 
                 <div class="opacity-55 text-sm line-clamp-1">
-                    {{ card.description }}
+                    {{ deal.description }}
                 </div>
 
-                <UButton color="blue" class="w-full mt-3 group" variant="ghost"> 
+                <UButton color="blue" class="w-full mt-3 group" variant="ghost" @click="set(deal)"> 
                     <span class="font-bold">More details</span>
 
                     <Icon name="material-symbols:arrow-right-alt-rounded" class="group-hover:translate-x-2 transition"/>
@@ -72,6 +98,6 @@
             </div>
         </div>
        
-        
+        <Slideover />
     </div>
 </template>
