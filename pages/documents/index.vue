802868ce4b56b72099b179887f8faacf64cbea1d
dsabirbaev <davran9559@gmail.com>
2024-01-27 13:38:39 +0500

    fixed status query

--- pages/documents/index.vue
+++ pages/documents/index.vue
@@ -1,28 +1,25 @@
+<script setup lang="ts">
+    import { useMutation } from '@tanstack/vue-query'
+    import { COLLECTION_DEALS, DB_ID } from '~/constants'
+    import { DATABASE } from '~/libs/appwrite'
+    import { useStatus } from '~/query/use-status'
+    import { useCurrentDealStore } from '~/store/current-deal.store'
+    import type { IColumn, IDeal } from '~/types'
 
-<script lang="ts" setup>
-    
-    import { useMutation } from '@tanstack/vue-query';
-    import { COLLECTION_DEALS, DB_ID } from '~/constants';
-    import { DATABASE } from '~/libs/appwrite';
-    import { useStatusQuery } from '~/query/use-status';
-    import { useCurrentDealStore } from "~/store/current-deal.store"
-    import type { IColumn, IDeal } from '~/types';
-   
+    definePageMeta({ layout: 'documents' })
+    useHead({ title: 'Documents | Jira software' })
 
-    definePageMeta({ layout: "documents" })
-    useHead({ title: "Documents | Jira software" })
-
-    const { data, isLoading, refetch } = useStatusQuery()
+    const { data, isLoading, refetch } = useStatus()
     const { set } = useCurrentDealStore()
 
     const dragCardRef = ref<IDeal | null>(null)
     const sourceColumnRef = ref<IColumn | null>(null)
     const isMoving = ref(false)
 
-    const {mutate, isPending} = useMutation({
+    const { mutate, isPending } = useMutation({
         mutationKey: ['moveCard'],
-        mutationFn: ({ docId, status } : {docId: string, status: string}) =>
-            DATABASE.updateDocument(DB_ID, COLLECTION_DEALS, docId, {status}),
+        mutationFn: ({ docId, status }: { docId: string; status: string }) =>
+            DATABASE.updateDocument(DB_ID, COLLECTION_DEALS, docId, { status }),
         onSuccess: () => refetch(),
     })
 
@@ -38,63 +35,90 @@
 
     const handleDrop = (column: IColumn) => {
         isMoving.value = false
-        if(dragCardRef.value && sourceColumnRef.value){
-            mutate({ docId: dragCardRef.value.$id, status: column.id})
+        if (dragCardRef.value && sourceColumnRef.value) {
+            mutate({ docId: dragCardRef.value.$id, status: column.id })
         }
     }
-
-    
 </script>
 
 <template>
-    <div v-if="isLoading" class="grid grid-cols-4 gap-2 mt-12">
-
-        <USkeleton class="h-12 dark:bg-gray-900 bg-gray-100" />
+	<div class="grid grid-cols-4 gap-2 mt-12" v-if="isLoading">
 		<USkeleton class="h-12 dark:bg-gray-900 bg-gray-100" />
 		<USkeleton class="h-12 dark:bg-gray-900 bg-gray-100" />
 		<USkeleton class="h-12 dark:bg-gray-900 bg-gray-100" />
+		<USkeleton class="h-12 dark:bg-gray-900 bg-gray-100" />
 
-        <UiDealsLoader />
-        <UiDealsLoader />
-        <UiDealsLoader />
-        <UiDealsLoader />
-    </div>
+		<UiDealsLoader />
+		<UiDealsLoader />
+		<UiDealsLoader />
+		<UiDealsLoader />
+	</div>
 
-    <div class="grid grid-cols-4 gap-2 mt-12" v-else>
-        
-        <div v-for="column in data" :key="column.id" 
-             @dragover="handleDragOver" 
-             @drop="() => handleDrop(column)"
-             class="px-1"
-             :class="isMoving && 'border-l-2 border-r-2 border-dotted h-screen dark:border-gray-900 border-gray-200'">
-            <UButton class="w-full h-12" color="blue" variant="outline">
-                <div class="flex items-center space-x-2">
-                    <span class="font-bold">{{ column.name }}</span>
-                    <span class="text-sm text-neutral-500">{{ column.items.length }}</span>
-                </div>
-            </UButton>
+	<div class="grid grid-cols-4 gap-2 mt-12" v-else>
+		<div
+			v-for="column in data"
+			:key="column.id"
+			@dragover="handleDragOver"
+			@drop="() => handleDrop(column)"
+			class="px-1"
+			:class="
+				isMoving &&
+				'border-l-2 border-r-2 border-dotted h-screen dark:border-gray-900 border-gray-200'
+			"
+		>
+			<UButton class="w-full h-12" color="blue" variant="outline">
+				<div class="flex items-center space-x-2">
+					<span class="font-bold">{{ column.name }}</span>
+					<span class="text-sm text-neutral-500">{{
+						column.items.length
+					}}</span>
+				</div>
+			</UButton>
 
-            <SharedCreateDeal :status="column.id" :refetch="refetch"/>
+			<SharedCreateDeal :status="column.id" :refetch="refetch" />
 
-            <div class="my-3 bg-gray-900 rounded-md p-2" v-for="deal in column.items" :key="deal.$id" role="button" 
-                 draggable="true" 
-                 @dragstart="() => handleDragStart(deal, column)"
-                 :class="isPending && 'opacity-50 cursor-not-allowed'">
+			<div
+				class="my-3 dark:bg-gray-900 bg-gray-100 rounded-md p-2 animation"
+				v-for="deal in column.items"
+				:key="deal.$id"
+				role="button"
+				draggable="true"
+				@dragstart="() => handleDragStart(deal, column)"
+				:class="isPending && 'opacity-50 cursor-not-allowed'"
+			>
+				<div
+					class="flex items-center space-x-2"
+					role="button"
+					@click="set(deal)"
+				>
+					<span class="font-bold text-lg uppercase">{{ deal.name }}</span>
+				</div>
 
-                <div class="flex items-center space-x-2" role="button">
-                    <span class="font-bold text-lg uppercase">{{ deal.name }}</span>
-                </div>
+				<UDivider class="my-3" />
 
-                <UDivider class="my-3" />
+				<div class="opacity-55 text-sm line-clamp-1">
+					{{ deal.description }}
+				</div>
+			</div>
+		</div>
 
-                <div class="opacity-55 text-sm line-clamp-1">
-                    {{ deal.description }}
-                </div>
+		<Slideover />
+	</div>
+</template>
 
-                
-            </div>
-        </div>
-       
-        <Slideover />
-    </div>
-</template>~/query/use-status
+<style scoped>
+@keyframes show {
+	from {
+		transform: scale(0.5) translateY(-30px);
+		opacity: 0.4;
+	}
+	to {
+		transform: scale(1) translateY(0);
+		opacity: 1;
+	}
+}
+
+.animation {
+	animation: show 0.3s ease-in-out;
+}
+</style>
--- query/use-status.ts
+++ query/use-status.ts
@@ -1,47 +1,42 @@
+import { useQuery } from '@tanstack/vue-query'
+import { Query } from 'appwrite'
+import { COLLECTION_DEALS, DB_ID, status } from '~/constants'
+import { DATABASE } from '~/libs/appwrite'
+import type { IColumn, IDeal } from '~/types'
+import { useAuthStore } from './../store/auth.store'
 
-import { useQuery } from "@tanstack/vue-query";
+export const useStatus = () => {
+	const { currentUser } = useAuthStore()
 
-import { Query } from "appwrite";
-import { COLLECTION_DEALS, DB_ID, status } from "~/constants";
-import { DATABASE } from "~/libs/appwrite";
+	return useQuery({
+		queryKey: ['deals'],
+		queryFn: () =>
+			DATABASE.listDocuments(DB_ID, COLLECTION_DEALS, [
+				Query.equal('userId', currentUser.id),
+			]),
+		select: data => {
+			const newBoard: IColumn[] = status.map(item => ({
+				...item,
+				items: [],
+			}))
 
-import { useAuthStore } from "~/store/auth.store";
-import type { IColumn, IDeal } from '~/types';
+			const deals = data.documents as unknown as IDeal[]
 
-export const useStatusQuery = () => {
+			for (const deal of deals) {
+				const column = newBoard.find(item => item.id === deal.status)
 
-    const { currentUser } = useAuthStore();
-    
-    return useQuery({
-        queryKey: ['deals'],
-        queryFn: () => 
-            DATABASE.listDocuments(DB_ID, COLLECTION_DEALS, [
-                Query.equal('userId', currentUser.id),
-            ]),
-
-        select: data => {
-            const newBoard: IColumn[] = status.map(item => ({
-                ...item,
-                items: [],
-            }))
-
-            const deals = data.documents as unknown as IDeal[]
-
-            for(const deal of deals){
-                const column = newBoard.find(item => item.id === deal.status)
-
-                if(column) {
-                    column.items.push({
-                        $createdAt: deal.$createdAt,
+				if (column) {
+					column.items.push({
+						$createdAt: deal.$createdAt,
 						name: deal.name,
 						description: deal.description,
 						status: deal.status,
 						$id: deal.$id,
-                    })
-                }
+					})
+				}
+			}
 
-                return newBoard;
-            }
-        },
-    })
+			return newBoard
+		},
+	})
 }
